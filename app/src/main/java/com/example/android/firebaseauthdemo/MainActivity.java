626ca0451376bd3b3b55b32b91946379349fe5a2
Benjamin Kyhn <benkyhn@hotmail.com>
2020-05-02 22:23:33 +0200

    Added another activity

--- app/src/main/java/com/example/android/firebaseauthdemo/MainActivity.java
+++ app/src/main/java/com/example/android/firebaseauthdemo/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +27,7 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class MainActivity extends AppCompatActivity {
     private SignInButton signInButton;
     private Button signOutButton;
+    private Button btnNext;
     private GoogleSignInClient mGoogleSignInClient;
     private  String TAG = "MainActivity";
     private FirebaseAuth mAuth;
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         signInButton = findViewById(R.id.sign_in_button);
         signOutButton = findViewById(R.id.sign_out_button);
+        btnNext = findViewById(R.id.btnNext);
         mAuth = FirebaseAuth.getInstance();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -65,6 +66,11 @@ public class MainActivity extends AppCompatActivity {
                 signOutButton.setVisibility(View.INVISIBLE);
             }
         });
+    }
+
+    public void next(View v){
+        Intent myIntent = new Intent(this, SecondActivity.class);
+        startActivity(myIntent);
     }
 
     private void signIn(){
